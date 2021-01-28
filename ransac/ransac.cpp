c1b90c1828e5edc50a18ebfc158c1cae011246b7
gregooze <gvreergo@gmail.com>
2021-01-28 15:09:15 -0800

    Auto stash before merge of "master" and "origin/master"

--- ransac/ransac.cpp
+++ ransac/ransac.cpp
@@ -1,5 +1,6 @@
 #include <set>
 #include <opencv2/core/core.hpp>
+#include <opencv2/sfm/projection.hpp>
 #include "ransac.hpp"
 
 //______________________CONSTRUCTOR & DESTRUCTOR_____________________
@@ -8,27 +9,36 @@ Ransac::Ransac(std::tuple< std::vector<cv::KeyPoint> , std::vector<cv::KeyPoint>
     this->n = nb_point; // number of points necessary to build a model
     this->max_error = error;  //max error value for a point to be considered an inlier
     this->w_goal = w; //minimum inlier rate wanted
-    this->ptr_frame = ptr_f; //pointer on the frame where we will stack data
-    this->nb_samples = this->get<2>(data)::size;
+    this->ptr_frame = ptr_f; //pointer on the frame containing the img Frame is a data container
+    this->nb_samples = std::get<2>(datapoint).size();
+    //need to init this->inlier , best_points and best_transform
 }
 
 Ransac::~Ransac(void){}
 
 //______________________MAIN CLASS FUNCTION _____________________
-void Ransac::apply_ransac(){
-  std::vector<cv::KeyPoint> temp_points;
-  long temp_w = 0; //current iteration w value
+cv::Mat Ransac::apply_ransac(){
+
+  uint64_t cpt_attempt = 0 , max_attempt = 500;
+  std::vector<std::tuple<cv::KeyPoint, cv::KeyPoint, cv::DMatch> > temp_point;
+  long temp_w = 0 , max_w = 0; //current iteration w value
   cv::Mat transform ; // transform created with pair of keypoints
-  while( temp_w < w_goal) {
-    temp_points = this->get_n_keypoints();
-    transform = this->transform_from_keypoints(keypoints);
-    temp_w = this->check_inliers(transform);
-    if() temp_w > w ) {
-      w = temp_w;
-      best_points = keypoints;
-      best_transform = transform
+
+  while( this->best_w < this->w_goal && cpt_attempt < max_attempt) {
+    temp_point = this->get_n_KeyPoints();
+    transform = this->transform_from_KeyPoints(temp_point);
+    temp_w = this->check_inliers(transform) / this-> nb_samples;
+
+    if( temp_w > max_w ) {
+      this->best_w = temp_w;
+      this->best_points = temp_point;
+      this->best_transform = transform;
     }
   }
+  if(cpt_attempt >= max_attempt){
+    std::cout<< "max_attempt reached and w_goal could not be reached, best_w = "<< this->best_w << std::endl << "best_points and best_transform are memorized"<<std::endl;
+  }
+  return this->best_transform;
 }
 
 //_____________________UTILITY FUNCTIONS__________________
@@ -41,56 +51,63 @@ std::vector <std::tuple< cv::KeyPoint , cv::KeyPoint , cv::DMatch > > Ransac::ge
 
   for(i = 0 ; i < n ; i ++){
     do{
-      temp = rand() % get<2>(data)::size;  // temp in the range 0 to length of DMatch
+      temp = rand() % std::get<2>(data).size() ;  // temp in the range 0 to length of DMatch
     }while (used_val.find(temp) != used_val.end() ); //as long as temp is already in used_val
-    used_val.add(temp);
-    res.insert(Ransac[temp]);
+    used_val.insert(temp);
+    res.push_back(this->operator[](temp));
   }
   return res;
 }
 
-cv::Mat Ransac::transform_from_keypoints( std::vector< cv::KeyPoint > kp ){
+cv::Mat Ransac::transform_from_KeyPoints( std::vector<std::tuple<cv::KeyPoint, cv::KeyPoint, cv::DMatch> > kp ){
   /*create homogenous transform matrix from n keypoints*/
-  cv::Point2f X , Y ;
-  X = kp.at(0);
-  Y = kp.at(1);
+  std::tuple<cv::KeyPoint, cv::KeyPoint, cv::DMatch>  temp_tuple ;
+  cv::Mat X,Y;
+  for(int i = 0 ; i < kp.size() ; i ++){
+      temp_tuple = kp.at(i);
+      //convert Point2f value of keypoints contained in tuple into a homogenous vector
+      //cv::sfm::euclideanToHomogeneous( std::get<1>(temp_tuple).pt , Y)
+      //cv::sfm::euclideanToHomogeneous( std::get<0>(temp_tuple).pt , X)
+
+
+  }
 }
 
 
 
-int Ransac::check_inliners(cv::Mat trans){
+int Ransac::check_inliers(cv::Mat trans){
   /*iterate over data Dmatch to check distance and add inlier if
   distance between point after appllying transformation < max_error */
   uint64_t i = 0;
   float distance;
   int cpt_inlier = 0;
-  std::vector< std::pair <cv::KeyPoint , cv::KeyPoint> > temp_inlier ; // data points whose reprojection error is under max_eror value
-  for (it = get<2>(data).begin(); it < get<2>(data).end() ; it++){
-      if(it*::distance < max_error){
-        temp_inlier.add(Ransac[i]);
-        cpt_inlier++;
-      }
-      i++;
+  std::vector< std::tuple< cv::KeyPoint , cv::KeyPoint , cv::DMatch > > temp_inlier ; // data points whose reprojection error is under max_eror value
+  for (uint64_t i = 0 ; i < (std::get<2>(data)).size(); i++){
+      //if(this-> data.distance < max_error){
+      //  temp_inlier.push_back(this->operator[](i));
+      //  cpt_inlier++;
+      //}
+      //i++;
   }
-  if(temp_inlier::size > inlier::size) {
-    inlier = temp_inlier;
+  if(temp_inlier.size() > this->inlier.size()) {
+    this->inlier = temp_inlier;
   }
-  return temp_inlier::size;
+  return temp_inlier.size();
 }
 
 
 
 //______________________OPERATOR OVERLOAD_____________________
-//Allow an easy access of data in form of pairs
 std::tuple< cv::KeyPoint , cv::KeyPoint , cv::DMatch > Ransac::operator[](uint64_t index){
+  //Allow an easy access of data in form of pairs
   //maybe problem de pointeurs à cet endroit à verifier
   //accesseur quand meme pratique on va pas se mentir permet de retourner les matched points et leur dmatch
   std::tuple< cv::KeyPoint , cv::KeyPoint , cv::DMatch > res ;
   cv::DMatch match = std::get<2>(data)[index] ;
   std::get<2>(res) = match ;
-  cv::KeyPoint temp_point = std::get<0>(data)[dmatch.queryIdx] ;
+  cv::KeyPoint temp_point = std::get<0>(data)[match.queryIdx] ;
   std::get<0>(res) = temp_point;
-  temp_point = std::get<1>(data)[dmatch.trainIdx] ;
+  temp_point = std::get<1>(data)[match.trainIdx] ;
   std::get<1>(res) = temp_point ;
 
   return res;
--- ransac/test.cpp
+++ ransac/test.cpp
@@ -1,39 +1,22 @@
-#include <opencv2/highgui.hpp>
 #include <iostream>
+#include <vector>
+#include <tuple>
 
-using namespace cv;
-using namespace std;
+int main(){
+  std::vector<uint64_t> vec1;
+  std::vector<uint64_t> vec2;
 
-int main(int argc, char* argv[])
-{
-vector<float> vec{0.1,0.9,0.2,0.8,0.3,0.7,0.4,0.6,0.5,1};
+  for(int i = 0 ; i <5 ; i++){
+    vec1.push_back(i);
+    vec2.push_back(5+i);
+  }
 
-Mat m1( vec );
-imshow("m1",m1);
-waitKey();
+  std::tuple< std::vector<uint64_t> , std::vector<uint64_t> > tup;
 
-Mat m2( 1,vec.size(), CV_32FC1,vec.data());
-imshow("m2",m2);
-waitKey();
+  std::get<0>(tup) = vec1;
+  std::get<2>(tup) = vec2;
 
-Mat1f m3( vec.size(), 1, vec.data());
-imshow("m3",m3);
-waitKey();
+  std::cout<<std::get<0>(tup)[1]<<std::endl;
 
-Mat1f m4( 1, vec.size(), vec.data());
-imshow("m4",m4);
-waitKey();
-
-cout << "as seen below all Mat and vector use same data" << endl;
-cout << vec[0] << endl;
-m1 *= 2;
-cout << vec[0] << endl;
-m2 *= 2;
-cout << vec[0] << endl;
-m3 *= 2;
-cout << vec[0] << endl;
-m4 *= 2;
-cout << vec[0] << endl;
-
-return 0;
+  return 0;
 }
